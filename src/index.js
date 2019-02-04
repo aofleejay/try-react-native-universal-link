8a7d7b450b2a4036b06b48af233e05e3917036ca
aofleejay <aofleejay@hotmail.com>
2019-02-04 12:47:47 +0700

    :sparkles: Add apple-app-site-association

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={App} />
+    </Switch>
+  </BrowserRouter>
+)
+
+ReactDOM.render(<Routes />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
