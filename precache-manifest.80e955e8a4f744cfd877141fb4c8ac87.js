40c8429d2ed3ea8e83f6a144a3bff29d980039fb
aofleejay <aofleejay@hotmail.com>
2019-02-04 10:15:35 +0700

    Updates

---
+++ precache-manifest.80e955e8a4f744cfd877141fb4c8ac87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "461fd1839e3950ce77e6",
+    "url": "/static/js/main.461fd183.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "461fd1839e3950ce77e6",
+    "url": "/static/css/main.2dcf9dd2.chunk.css"
+  },
+  {
+    "revision": "45682480f54d778afb9dd222ea78c46a",
+    "url": "/index.html"
+  }
+];
